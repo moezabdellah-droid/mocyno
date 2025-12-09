6df71db07102bf9839ecaae144901de4e6a73d7d
moezabdellah-droid <moezabdellah@mocyno.com>
2025-12-09 14:12:34 +0100

    fix(admin): remove custom UserMenu to fix router context error

--- admin/src/MyLayout.tsx
+++ admin/src/MyLayout.tsx
@@ -1,35 +1,10 @@
 // @ts-nocheck
-import type { LayoutProps, AppBarProps, UserMenuProps } from 'react-admin';
-import { Layout, AppBar, UserMenu, Logout } from 'react-admin';
-import { MenuItem, ListItemIcon, ListItemText } from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
-import { useNavigate } from 'react-router-dom';
+import type { LayoutProps, AppBarProps } from 'react-admin';
+import { Layout, AppBar } from 'react-admin';
 import React from 'react';
 
-// Custom User Menu Item to link to Profile
-const ProfileMenu = () => {
-    const navigate = useNavigate();
-
-    return (
-        <MenuItem onClick={() => navigate('/profile')}>
-            <ListItemIcon>
-                <SettingsIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText>Mon Profil</ListItemText>
-        </MenuItem>
-    );
-};
-
-// Custom User Menu including Profile and default Logout
-const MyUserMenu = (props: UserMenuProps) => (
-    <UserMenu {...props}>
-        <ProfileMenu />
-        <Logout />
-    </UserMenu>
-);
-
-// Custom AppBar using MyUserMenu
-const MyAppBar = (props: AppBarProps) => <AppBar {...props} userMenu={<MyUserMenu />} />;
+// Custom AppBar (can be customized later if needed)
+const MyAppBar = (props: AppBarProps) => <AppBar {...props} />;
 
 // Custom Layout using MyAppBar
 const MyLayout = (props: LayoutProps) => <Layout {...props} appBar={MyAppBar} />;
