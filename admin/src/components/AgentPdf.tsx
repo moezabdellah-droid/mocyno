a29081fd6ad1928d06e40dcfb9d3b1bc86e145fd
moezabdellah-droid <moezabdellah@mocyno.com>
2025-12-09 20:38:02 +0100

    fix(admin): make badge background transparent to show logo

--- admin/src/components/AgentPdf.tsx
+++ admin/src/components/AgentPdf.tsx
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
         padding: 10,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff'
+        backgroundColor: 'transparent'
     },
     badgeHeaderRed: {
         position: 'absolute',
